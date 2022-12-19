4ee76faeefe7a34eba8c95810e9e95e9487c8beb
rami chabchoub <ramichabchoub.rc@gmail.com>
2022-12-19 12:25:54 +0100

    docker with mongodb

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import express from "express"
+import mongoose from "mongoose";
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.get('/', (req, res) => res.send('<h1>Hello rami!!! test</h1>'));
+//connect to mongodb
+const DB_USER = 'root'
+const DB_PASSWORD = 'example'
+const DB_PORT = 27017
+const DB_HOST = 'mongo'
+
+const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`
+mongoose.connect(URI).then(() => console.log("connected to mongodb ✅"))
+    .catch((err) => console.log("fail to connect mongodb ⚠️"))
+
+app.get('/', (req, res) => res.send('<h1>Hello rami!!! test!!</h1>'));
 
 app.listen(PORT, () => console.log(`app is running on port ${PORT}`));
