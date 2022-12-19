047cdf08fe2c7e8dcea17bca9114be9c4446b80b
rami chabchoub <ramichabchoub.rc@gmail.com>
2022-12-19 13:58:46 +0100

    docker with redis

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import express from "express"
 import mongoose from "mongoose";
+import { createClient } from 'redis';
 
+//init app
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+//connect to redis
+const REDIS_PORT = 6379
+const REDIS_HOST = 'redis'
+// const redisClient = createClient();
+const redisClient = createClient({
+    url: `redis://${REDIS_HOST}:${REDIS_PORT}`
+});
+redisClient.on('error', (err) => console.log('Redis Client Error ⛔', err));
+await redisClient.on('connect', () => console.log('Connected to redis ✅'));
+await redisClient.connect();
 
 //connect to mongodb
 const DB_USER = 'root'
@@ -12,8 +25,16 @@ const DB_HOST = 'mongo'
 
 const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`
 mongoose.connect(URI).then(() => console.log("connected to mongodb ✅"))
-    .catch((err) => console.log("fail to connect mongodb ⚠️"))
+    .catch((err) => console.log("fail to connect mongodb ⛔"))
 
-app.get('/', (req, res) => res.send('<h1>Hello rami!!! test!!</h1>'));
+app.get('/', (req, res) => {
+    redisClient.set('products', 'Products ...🔥')
+    res.send('<h1>Hello rami!!! test!!</h1>')
+});
+
+app.get('/data', async (req, res) => {
+    const products = await redisClient.get('products')
+    res.send(`<h1>Hello rami!!! test!!</h1><h2>${products}</h2>`)
+});
 
 app.listen(PORT, () => console.log(`app is running on port ${PORT}`));
